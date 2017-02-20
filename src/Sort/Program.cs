e97e20aa0f3b95b8cd915f6b6cd89098409f76a3
Ricardo Bartolomeu <r3dbag@gmail.com>
2017-02-20 18:19:25 +0000

    Weak first attempt.

--- src/Sort/Program.cs
+++ src/Sort/Program.cs
@@ -11,15 +11,19 @@ namespace Sort
         private static void Main()
         {
             //const int length = 13000000;
-            const int length = 30000000;
+            //const int length = 10000000;
+            const int length = 100000;
 
             var data1 = Enumerable.Range(0, length).Reverse().Select(x => (x % 2 == 0 ? -1 : 1) * x).ToArray();
             var data2 = Enumerable.Range(0, length).Reverse().Select(x => (x % 2 == 0 ? -1 : 1) * x).ToArray();
             var data3 = Enumerable.Range(0, length).Reverse().Select(x => (x % 2 == 0 ? -1 : 1) * x).ToArray();
+            var data4 = Enumerable.Range(0, length).Reverse().Select(x => (x % 2 == 0 ? -1 : 1) * x).ToArray();
 
             Measure(() => SortCpu.Compute1(data1), false, length, "CPU: Library!");
             Measure(() => SortCpu.Compute2(data2), false, length, "CPU: Parallel Linq!");
             Measure(() => SortCpu.Compute3(data3), false, length, "CPU: Custom!");
+
+            Measure(() => SortGpu.Compute1(data4), true,  length, "GPU: Library!");
 
             Console.ReadLine();
         }
@@ -27,6 +31,7 @@ namespace Sort
         private static void Measure(Func<IEnumerable<int>> func, bool isGpu, int length, string description)
         {
             const string format = "{0,9}";
+            const int resultsToDisplay = 12;
 
             Func<Stopwatch, string> formatElapsedTime = w => w.Elapsed.TotalSeconds >= 1
                 ? string.Format(CultureInfo.InvariantCulture, format + "  (s)", w.Elapsed.TotalSeconds)
@@ -54,17 +59,17 @@ namespace Sort
             var result1 = func();
             sw1.Stop();
 
-            Console.WriteLine(new string('-', 49));
+            Console.WriteLine(new string('-', 88));
             Console.WriteLine(description);
             consoleColor(!IsValid(result1, 100));
-            Console.WriteLine("{0,9} - {1} - {2} [Cold]", string.Join(", ", result1.Take(5)), formatElapsedTime(sw1), bandwidth(sw1));
+            Console.WriteLine("{0,9} - {1} - {2} [Cold]", string.Join(", ", result1.Take(resultsToDisplay)), formatElapsedTime(sw1), bandwidth(sw1));
             Console.ResetColor();
 
             var sw2 = Stopwatch.StartNew();
             var result2 = func();
             sw2.Stop();
             consoleColor(!IsValid(result2, 100));
-            Console.WriteLine("{0,9} - {1} - {2} [Warm]", string.Join(", ", result2.Take(5)), formatElapsedTime(sw2), bandwidth(sw2));
+            Console.WriteLine("{0,9} - {1} - {2} [Warm]", string.Join(", ", result2.Take(resultsToDisplay)), formatElapsedTime(sw2), bandwidth(sw2));
             Console.ResetColor();
         }
 
---
+++ src/Sort/SortGpu.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using Alea;
+using Alea.Parallel;
+
+namespace Sort
+{
+    // Todo: Use IComparable<T>
+    // Todo: Use Blocks?!
+    internal static class SortGpu
+    {
+        internal static int[] Compute1(int[] array) /*where T : IComparable<T>*/
+        {
+            var steps = array.Length % 2 == 0
+                ? array.Length / 2
+                : array.Length / 2 + 1;
+
+            var gpu = Gpu.Default;
+
+            var inputLength = array.Length;
+            var inputMemory = gpu.Allocate(array);
+
+            gpu.For(0, array.Length, i =>
+            {
+                for (var k = 0; k < steps; k++)
+                {
+                    if (i < inputLength - 1)
+                    {
+                        var c = inputMemory[i + 0];
+                        var n = inputMemory[i + 1];
+
+                        if (i % 2 == 0 && c > n)
+                        {
+                            Exchange(inputMemory, i, i + 1);
+                        }
+
+                        if (i % 2 != 0 && c > n)
+                        {
+                            Exchange(inputMemory, i, i + 1);
+                        }
+
+                        DeviceFunction.SyncThreads();
+                    }
+                }
+            });
+
+            return Gpu.CopyToHost(inputMemory);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        // ReSharper disable once SuggestBaseTypeForParameter
+        private static void Exchange<T>(T[] array, int c, int n)
+        {
+            var temp = array[c];
+            array[c] = array[n];
+            array[n] = temp;
+        }
+    }
+}
